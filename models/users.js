93f0c14a7fb39c580724444fdb76e5e7980ccc1d
JudeRanidu <juderanidu96@gmail.com>
2022-01-08 22:12:00 +0530

    HTTP implementation completed

---
+++ models/users.js
@@ -0,0 +1,39 @@
+var mongoose =  require('mongoose')
+var Schema = mongoose.Schema
+
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var userSchema = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+/*var userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+})*/
+
+module.exports = mongoose.model('User', userSchema)
